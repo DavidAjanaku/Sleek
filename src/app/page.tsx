b63cfe82e0e6746b4de24ae628e64b66abe21993
David Ajanaku <davidajanaku@Davids-MacBook-Pro.local>
2025-08-15 10:32:17 +0100

    Update HeroSection and serum image section with responsive height classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,20 @@ export default function Home() {
       <HeroSection/>
       <FeaturesSection/>
       <ProductGrid/>
-      <div className='px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 h-2/3'>
+       <div className='px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-8 md:py-12 lg:py-16 my-8 md:my-12 lg:my-16 h-96 sm:h-80 md:h-96 lg:h-2/3'>
         <img
           src="/assets/Frame.png"
           className="w-full h-full object-cover transition-all duration-300"
         />
       </div>
       <Testimonial/>
-     <div className='px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-8 md:py-12 lg:py-16 my-8 md:my-12 lg:my-16 h-2/3'>
-  <img
-    src="/assets/serum.png"
-    className="w-full h-full object-cover transition-all duration-300"
-  />
-</div>
+      {/* Updated serum image section with bigger mobile size */}
+      <div className='px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-8 md:py-12 lg:py-16 my-8 md:my-12 lg:my-16 h-96 sm:h-80 md:h-96 lg:h-2/3'>
+        <img
+          src="/assets/serum.png"
+          className="w-full h-full object-cover transition-all duration-300"
+        />
+      </div>
       <Footer/>
     </div>
   );
